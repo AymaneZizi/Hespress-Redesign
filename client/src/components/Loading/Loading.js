e4bd7b451e01b4695bef05c3e494eb5043977e8c
zakaria <zakariaalazrake@gmail.com>
2019-05-30 00:26:26 +0000

    remove ignored files

--- client/src/components/Loading/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Loading.css'
-
-export default function Loading(props) {
-    return (
-        <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-    )
-}
--- client/src/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer'
-import errorReducer from './errorReducer.js'
-
-export default combineReducers({
-    auth: authReducer,
-    error: errorReducer
-})
